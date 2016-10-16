e8786449e381cca0821a241eac0fcaa94e5ec891
dpqhuy <dpqhuy@gmail.com>
2016-10-16 14:37:01 +0700

    update

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,9 @@
 class CartsController < ApplicationController
   def show
   	@order = current_order
+  	if @order.nil?
+  		@order = Order.new
+  	end
   	@order_items = @order.order_items
   end
 end
